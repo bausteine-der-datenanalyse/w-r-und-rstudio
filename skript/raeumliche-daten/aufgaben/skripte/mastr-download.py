0f60438aed98188e4634afb3f5bc41f8856ff1b5
FrancaHollmann <franca.hollmann@hs-bochum.de>
2025-10-06 17:05:36 +0200

    WIP

--- skript/raeumliche-daten/aufgaben/skripte/mastr-download.py
+++
@@ -1,8 +0,0 @@
-# Marktstammdatenregister herunterladen und in SQLite-Datenbank speichern
-#
-# Siehe 
-# - https://github.com/OpenEnergyPlatform/open-MaStR
-
-from open_mastr import Mastr
-db = Mastr()
-db.download()
